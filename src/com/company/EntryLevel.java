19ef1bc6ab867dcd3ee7c32706a561026d744ae6
程鹏兴 <chengpengxing@chengpengxingdeMacBook-Pro.local>
2020-12-13 22:44:31 +0800

    Add the flow control

--- src/com/company/EntryLevel.java
+++ src/com/company/EntryLevel.java
@@ -55,31 +55,115 @@ public class EntryLevel {
         s3 = "Baidu";
         System.out.printf("s4 = %s\n", s4); // 是输出 Google 还是 Baidu？ Google
         // 数组是引用类型: 整形数组和字符串数组的区别
-        int ns[]  = new int[] {11,22,33}; //如果在定义数组变量的同时初始化数组内容，就不能再指定数组大小了
+        int[] ns = new int[] {11,22,33}; //如果在定义数组变量的同时初始化数组内容，就不能再指定数组大小了
         int itmp = ns[1];
         ns[1] = 99;
         System.out.printf("itmp = %d\n", itmp); // itmp 是 99 还是 22 ？ 22
         String[] names = {"ABC", "XYZ", "zoo"};
         String stmp = names[1];
         names[1] = "cat";
-        System.out.printf("stmp = %s\n",stmp); // stmp 是"XYZ"还是"cat"?  XYZ  重点2：这和 Python 很不一样
+        System.out.printf("stmp = %s\n",stmp); // stmp 是"XYZ"还是"cat"?  XYZ
 
         // Java源码的缩进不是必须的，但是用缩进后，格式好看，很容易看出代码块的开始和结束，缩进一般是4个空格或者一个tab。
         int score = 60;
         if (score > 90)
             System.out.println("excellent!");
-        System.out.println("soso......"); // 重点3：这句话居然输出了，这和 Python 很不一样
+            System.out.println("soso......"); // 重点2：这句话居然输出了，这和 Python 很不一样,充分证明 Java 不缩进
 
         // 输入
         // Scanner scanner = new Scanner(System.in);
         // System.out.println("Please input your age:");
         // int age = scanner.nextInt();
         // System.out.printf("age = %d\n", age);
-        int aa = 2;
-        int bb = aa;
-        aa = 3333;
-        System.out.println(bb);
+        cmpString();
+        switchTest();
+        overflowInt();  // 重点3-1：非常棒的测试
+        forCircle();
+    }
 
+    public void cmpString(){
+        String s1 = "hello";
+        String s2 = "HELLO".toLowerCase();
+        System.out.printf("s1 = %s, s2 = %s\n", s1, s2);
+        if (s1 == s2 ){
+            System.out.println("s1 = s2");
+        } else {
+            System.out.println("s1 != s2");
+        }
+        // 判断引用类型的变量内容是否相等，必须使用equals()方法
+        if (s1.equals(s2)){
+            System.out.println("s1 = s2");
+        } else {
+            System.out.println("s1 != s2");
+        }
+    }
+
+
+    public void switchTest(){
+        String fruit = "apple";
+        // case 语句有 "穿透性"
+        switch (fruit) {
+            case "apple":
+                System.out.println("Selected apple");
+                break;
+            case "mango":
+                System.out.println("Selected mango");
+                break;
+            default:
+                System.out.println("No fruit selected");
+                break;
+        }
+
+
+        // 从Java 12开始，switch语句升级为更简洁的表达式语法，
+        // 使用类似模式匹配（Pattern Matching）的方法，保证只有一种路径会被执行，并且不需要break语句：
+        // 注意新语法使用->，如果有多条语句，需要用{}括起来。不要写break语句，因为新语法只会执行匹配的语句，没有穿透效应。
+        switch (fruit) {
+            case "apple" -> System.out.println("Selected apple");
+            case "mango" -> {
+                System.out.println("Selected mango");
+                System.out.println("Good choice!");
+            }
+            default -> System.out.println("No fruit selected");
+        }
+
+        //大多数时候，在switch表达式内部，我们会返回简单的值。
+        //但是，如果需要复杂的语句，我们也可以写很多语句，放到{...}里，然后，用 yield 返回一个值作为switch语句的返回值
+        fruit = "orange";
+        int opt = switch (fruit) {
+            case "apple" -> 1;
+            case "pear", "mango" -> 2;
+            default -> {
+                int code = fruit.hashCode();
+                yield code; // switch语句返回值
+            }
+        };
+        System.out.println("opt = " + opt);
+    }
+
+    public void overflowInt(){
+        // 重点3：
+        // 表面上看，上面的while循环是一个死循环，但是，Java的int类型有最大值，达到最大值后，再加1会变成负数，结果，意外退出了while循环。
+        int sum = 0;
+        int n = 1;
+        while (n > 0) {
+            sum = sum + n;
+            n ++;
+        }
+        System.out.println(n); // -2147483648
+        System.out.println(sum);
+    }
+
+    public void forCircle(){
+        int[] ns = { 1, 4, 9, 16, 25 };
+        for (int i=0; i<ns.length; i++) {
+            System.out.println(ns[i]);
+        }
+        // 很多时候，我们实际上真正想要访问的是数组每个元素的值。Java还提供了另一种 for each循环，它可以更简单地遍历数组
+        System.out.println("===for each===");
+        for (int n : ns) {
+            System.out.println(n);
+        }
     }
 
 }
