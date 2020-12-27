1b329ed99330796a7c6e6d1d96ad7790ef493c15
程鹏兴 <chengpengxing@chengpengxingdeMacBook-Pro.local>
2020-12-27 22:13:22 +0800

    Add the comments for override and overload

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -29,7 +29,9 @@ public class Student extends Person {
         return this.score;
     }
 
-    // Override 覆写父类的方法
+    // Override 覆写父类的方法：Override(覆写) 和Overload(重载)不同的是，
+    // 如果方法签名如果不同，就是Overload，Overload方法是一个新方法；
+    // 如果方法签名相同，并且返回值也相同，就是Override。
     @Override
     public void run(){
         System.out.println("In the Student.run");
